3927a02a92a3f8dcd60f9fb7a1c166dc8ac32d57
Usama Bashir <usamab@mudsoft.tech>
2025-04-11 14:50:23 +0500

    first commit

---
+++ config/google.template.php
@@ -0,0 +1,7 @@
+<?php
+// config/google.template.php
+return [
+  'clientId'     => 'YOUR_GOOGLE_CLIENT_ID',
+  'clientSecret' => 'YOUR_GOOGLE_CLIENT_SECRET',
+  'redirectUri'  => 'http://localhost:8000/example/oauth_callback.php?provider=google',
+];
